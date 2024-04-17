f6d4537f89ffbec01dab93b4da12802fc854dc70
Guliaev Maxim <maxim_@bk.ru>
2024-04-17 14:47:21 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FindYourTicket"
+include(":app")
+include(":core")
+include(":air_ticket")
+include(":air_ticket:feature_air_ticket_data")
+include(":air_ticket:feature_air_ticket_domain")
+include(":air_ticket:feature_air_ticket_presentation")
