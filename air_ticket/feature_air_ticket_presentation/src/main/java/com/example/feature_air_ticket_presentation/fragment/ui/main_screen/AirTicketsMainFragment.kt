1507aed8654d2bf8a0a416e8bdb58b1428de974d
Guliaev Maxim <maxim_@bk.ru>
2024-04-19 22:01:43 +0300

    refine offer recycler view

--- air_ticket/feature_air_ticket_presentation/src/main/java/com/example/feature_air_ticket_presentation/fragment/ui/main_screen/AirTicketsMainFragment.kt
+++ air_ticket/feature_air_ticket_presentation/src/main/java/com/example/feature_air_ticket_presentation/fragment/ui/main_screen/AirTicketsMainFragment.kt
@@ -3,26 +3,32 @@ package com.example.feature_air_ticket_presentation.fragment.ui.main_screen
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.feature_air_ticket_data.data_source.MainScreenDataSourceImpl
 import com.example.feature_air_ticket_data.repository.MainScreenRepositoryImpl
 import com.example.feature_air_ticket_domain.use_case.get_music_offer_list.GetMusicOfferListUseCaseImpl
 import com.example.feature_air_ticket_presentation.databinding.FragmentAirTicketMainBinding
+import com.example.feature_air_ticket_presentation.fragment.ui.main_screen.adapter.MusicOfferAdapter
+import com.example.feature_air_ticket_presentation.fragment.ui.main_screen.model.MusicOffer
 import com.example.feature_air_ticket_presentation.fragment.utils.MainFragmentViewModelFactory
 import com.example.feature_air_ticket_presentation.fragment.utils.SharedPreferences
 import ui.BaseFragment
 
-class AirTicketsMainFragment: BaseFragment() {
+class AirTicketsMainFragment : BaseFragment() {
 
     //TODO rid this horrible code using Dagger
     private val viewModel by lazy {
-        ViewModelProviders.of(this, MainFragmentViewModelFactory(GetMusicOfferListUseCaseImpl(
-            MainScreenRepositoryImpl(MainScreenDataSourceImpl())
-        ))).get(AirTicketsMainViewModel::class.java)
+        ViewModelProviders.of(
+            this, MainFragmentViewModelFactory(
+                GetMusicOfferListUseCaseImpl(
+                    MainScreenRepositoryImpl(MainScreenDataSourceImpl())
+                )
+            )
+        ).get(AirTicketsMainViewModel::class.java)
     }
 
     private var _binding: FragmentAirTicketMainBinding? = null
@@ -39,8 +45,7 @@ class AirTicketsMainFragment: BaseFragment() {
 
         setPersistedInput()
         setInputListener()
-
-        setMusicOfferRecycler()
+        getMusicOfferList()
 
         return binding?.root
     }
@@ -52,13 +57,14 @@ class AirTicketsMainFragment: BaseFragment() {
 
     private fun setInputListener() {
         binding?.apply {
-            etFrom.addTextChangedListener( object : TextWatcher {
+            etFrom.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(
                     s: CharSequence?,
                     start: Int,
                     count: Int,
                     after: Int
-                ) {}
+                ) {
+                }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
@@ -74,10 +80,21 @@ class AirTicketsMainFragment: BaseFragment() {
         binding?.etFrom?.setText(sharedPreferences?.getInput() ?: "")
     }
 
-    private fun setMusicOfferRecycler() {
+    private fun getMusicOfferList() {
         viewModel.getMusicOffersList()
-        viewModel.musicOfferList.observe(viewLifecycleOwner) {
-            Log.i("maxlog", it.toString())
+        viewModel.musicOfferList.observe(viewLifecycleOwner) { list ->
+            setMusicOfferRecycler(list)
+        }
+    }
+
+    private fun setMusicOfferRecycler(offerList: List<MusicOffer>) {
+        binding?.rvMusicOffer?.apply {
+            layoutManager = LinearLayoutManager(
+                activity,
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            adapter = MusicOfferAdapter(offerList)
         }
     }
 }
---
+++ air_ticket/feature_air_ticket_presentation/src/main/java/com/example/feature_air_ticket_presentation/fragment/ui/main_screen/adapter/MusicOfferAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.feature_air_ticket_presentation.fragment.ui.main_screen.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.feature_air_ticket_presentation.R
+import com.example.feature_air_ticket_presentation.databinding.RecyclerMusicTripItemBinding
+import com.example.feature_air_ticket_presentation.fragment.ui.main_screen.model.MusicOffer
+
+class MusicOfferAdapter(
+    private val offerList: List<MusicOffer>
+) : RecyclerView.Adapter<MusicOfferAdapter.MusicOfferViewHolder>() {
+
+    override fun onBindViewHolder(holder: MusicOfferViewHolder, position: Int) {
+        val item = offerList[position]
+
+        holder.apply {
+            title.text = item.title
+            town.text = item.town
+            price.text = item.price.value.toString()
+            when (item.musicOfferId) {
+                1 -> image.setImageResource(R.drawable.antwoord)
+                2 -> image.setImageResource(R.drawable.socrat)
+                3 -> image.setImageResource(R.drawable.lambapict)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MusicOfferViewHolder {
+        val itemBinding = RecyclerMusicTripItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return MusicOfferViewHolder(itemBinding)
+    }
+
+    override fun getItemCount(): Int = offerList.size
+
+    class MusicOfferViewHolder(
+        binding: RecyclerMusicTripItemBinding
+    ) : ViewHolder(binding.root) {
+        val image = binding.ivMusicianPicture
+        val title = binding.tvMusicianName
+        val town = binding.tvMusicianCity
+        val price = binding.tvPrice
+    }
+
+}
